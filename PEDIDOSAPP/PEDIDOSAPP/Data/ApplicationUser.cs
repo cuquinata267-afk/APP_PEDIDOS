fa6356593fc27d124cefd50d6a0dbfe8e80ee73d
nata romero <cuquinata267-afk>
2025-09-18 07:40:49 -0400

    3. Agregando DBContext

---
+++ PEDIDOSAPP/PEDIDOSAPP/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PEDIDOSAPP.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, StringLength(100)]
+        public string Nombre { get; set; }   // nombre real del usuario
+    }
+}
