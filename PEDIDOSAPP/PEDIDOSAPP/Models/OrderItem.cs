8cb5c32e1d52823308ed0fb36db4718f0904b6d4
nata romero <cuquinata267-afk>
2025-09-18 08:48:52 -0400

    Crear modelo OrderItem

---
+++ PEDIDOSAPP/PEDIDOSAPP/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PEDIDOSAPP.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Subtotal { get; set; }
+    }
+
+}
