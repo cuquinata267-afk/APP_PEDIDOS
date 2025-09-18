07c57cdca9e3f6548d22a210b7ab27013f986328
nata romero <cuquinata267-afk>
2025-09-18 08:45:15 -0400

    Crear Modelo Order

---
+++ PEDIDOSAPP/PEDIDOSAPP/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using PEDIDOSAPP.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace PEDIDOSAPP.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }      // FK a ApplicationUser.Id
+        public ApplicationUser User { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        [Required, StringLength(50)]
+        public string Status { get; set; } = "Pendiente";
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Total { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; }
+    }
+}
